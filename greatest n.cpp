d1ddefa234a19bf4d0bf3cbe592c4f9106b66dc7
sumona khatun <139578825+sumonaPSP@users.noreply.github.com>
2023-11-07 17:21:55 +0600

    Add files via upload
    #include <iostream>
    #include<conio.h>
    using namespace std;
    template <typename T>
    T findGreatest(T a, T b, T c)
    {
        if (a >= b && a >= c)
        {
            return a;
        }
        else if (b >= a && b >= c)
        {
            return b;
        }
        else
        {
            return c;
        }
    }
    int main()
    {
        int int1 = 5, int2 = 10, int3 = 3;
        char char1 = 'A', char2 = 'Z', char3 = 'M';
        float float1 = 3.14, float2 = 2.71, float3 = 4.2;
        int maxInt = findGreatest(int1, int2, int3);
        cout << "Max integer: " << maxInt << endl;
        char maxChar = findGreatest(char1, char2, char3);
        cout << "Max character: " << maxChar << endl;
        float maxFloat = findGreatest(float1, float2, float3);
        cout << "Max float: " << maxFloat << endl;
        getch();
    }

---
+++ greatest n.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include<conio.h>
+using namespace std;
+template <typename T>
+T findGreatest(T a, T b, T c)
+{
+    if (a >= b && a >= c)
+    {
+        return a;
+    }
+    else if (b >= a && b >= c)
+    {
+        return b;
+    }
+    else
+    {
+        return c;
+    }
+}
+int main()
+{
+    int int1 = 9, int2 = 19, int3 = 1;
+    char char1 = 'A', char2 = 'Z', char3 = 'M';
+    float float1 = 1.23, float2 = 3.56, float3 = 8.9;
+    int maxInt = findGreatest(int1, int2, int3);
+    cout << "Max integer: " << maxInt << endl;
+    char maxChar = findGreatest(char1, char2, char3);
+    cout << "Max character: " << maxChar << endl;
+    float maxFloat = findGreatest(float1, float2, float3);
+    cout << "Max float: " << maxFloat << endl;
+    getch();
+}
